5acd37157a723da216b987341cd194523f9786c5
Mücahit Kurtlar <mucahit@tutanota.com>
2024-06-27 20:08:21 +0300

    feat: Author page

--- src/routes/yazar/[author]/+page.ts
+++ src/routes/yazar/[author]/+page.ts
@@ -1,11 +1,59 @@
+import * as env from '$env/static/public';
+import { tokenStore } from '$lib/stores/user';
+import type { Entry, ErrorResponse, PaginationResponse } from '$lib/types';
+import { get } from 'svelte/store';
+
 export const prerender = false;
 
-export async function load({ params }) {
-	return {
-		author: params.author
-	};
+export async function load({ params, fetch, url }) {
+	try {
+		const page = Number(url.searchParams.get('page') || 1);
+		const perPage = Number(url.searchParams.get('perPage') || 10);
+		const fetchUrl = `${env.PUBLIC_API_URL}/users/${params.author}/entries?page=${page}&perPage=${perPage}`;
+
+		const token = get(tokenStore);
+		const response = await fetch(fetchUrl, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+
+		if (response.ok) {
+			const entries: PaginationResponse<Entry> = await response.json();
+			const authorData: AuthorData = {
+				author: params.author,
+				entries,
+				error: null
+			};
+
+			return authorData;
+		} else {
+			const error = await response.json();
+			const authorData: AuthorData = {
+				author: params.author,
+				entries: null,
+				error
+			};
+
+			return authorData;
+		}
+	} catch (e) {
+		console.error(e);
+		const error: ErrorResponse = {
+			error: 'İstek Hatası',
+			details: 'Girdiler yüklenirken bir hata oluştu.'
+		};
+
+		return {
+			author: params.author,
+			entries: [],
+			error
+		};
+	}
 }
 
 export type AuthorData = {
 	author: string;
+	entries: PaginationResponse<Entry> | null;
+	error: ErrorResponse | null;
 };
